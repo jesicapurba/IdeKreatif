0a5cd331c091b947b391e8de4e650a14739b587c
jesicapurba <jesica.purba30@smk.belajar.id>
2025-02-04 14:28:00 +0700

    menambahkan

--- posts.php
+++ posts.php
@@ -10,39 +10,39 @@ include '.includes/header.php';
                 <form method="POST" action="proses_post.php" enctype="multipart/form-data">
                     <div class="mb-3">
                         <label for="post_title" class="form-label">Judul Postingan</label>
-                        <input type="text" class="form-control" name="post_title" required>
-</div>
+                            <input type="text" class="form-control" name="post_title" required>
+                                </div>
 <div class="mb-3">
     <label for="formFile" class="form-label">Unggah Gambar</label>
-    <input class="form-control" type="file" name="image" accept="image/*"/>
-</div>
+        <input class="form-control" type="file" name="image" accept="image/*"/>
+            </div>
 <div class="mb-3">
     <label for="category_id" class="form-label">Kategori</label>
-    <select class="form-select" name="category_id" required>
-        <option value="" selected disabled>Pilih Salah Satu</option>
+         <select class="form-select" name="category_id" required>
+            <option value="" selected disabled>Pilih Salah Satu</option>
 
-        <?php
-        $query = "SELECT * FROM categories"; //query untuk mengambil data kategori
-        $result = $conn->query($query); //menjalankan query 
-        if ($result->num_rows > 0) { //jika terdapat data kategori
-            while ($row = $result->fetch_assoc()) { //iterasi setiap kategori
-                echo "<option value='" . $row["category_id"] . "'>" . $row["category_name"] . "</option>";
-            }
-        }
-        ?>
-        </select>
-    </div>
-    <div class="mb-3">
-        <label for="content" class="form-label">Konten</label>
+<?php
+    $query = "SELECT * FROM categories"; 
+        $result = $conn->query($query); 
+            if ($result->num_rows > 0) { 
+                 while ($row = $result->fetch_assoc()) { 
+                     echo "<option value='" . $row["category_id"] . "'>" . $row["category_name"] . "</option>";
+                        }
+                          }
+?>
+    </select>
+        </div>
+<div class="mb-3">
+    <label for="content" class="form-label">Konten</label>
         <textarea class="form-control" id="content" name="content" required></textarea>
-    </div>
-    <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
-                     </form>
-                  </div>
-               </div>
-           </div>
+             </div>
+                 <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                </form>
+              </div>
+          </div>
        </div>
-    </div>
+     </div>
+   </div>
 <?php
 include '.includes/footer.php';
 ?>
--- proses_post.php
+++ proses_post.php
@@ -40,3 +40,65 @@ if (isset($_POST['simpan'])) {
     header('Location: dashboard.php');
     exit();
 }
+
+if (isset($_POST['delete'])) {
+    $postID = $_POST['postID'];
+
+    $exec = mysqli_query($conn, "DELETE FROM posts WHERE id_post='$postID'");
+
+    if ($exec) {
+        $_SESSION['notification'] = [
+             'type' => 'primary',
+             'message' => 'Post successfully deleted.'
+        ];
+     } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Error deleting post: '. mysqli_error($conn)
+        ];
+    }
+    header('Location: dashboard.php');
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update'])) {
+    $postId = $_POST['post_id'];
+    $postTitle = $_POST['post_title'];
+    $content = $_POST['content'];
+    $categoryId = $_POST['category_id'];
+    $imageDir = "assets/img/uploads/";
+
+    if (!empty($_FILES["image_path"]["name"])) {
+        $imageName = $_FILES["image_path"]["name"];
+        $imagePath = $imageDir . $imageName;
+
+        move_uploaded_file($_FILES["image_path"]["tmp_name"], $imagePath);
+         $queryOldImage = "SELECT image_path FROM posts WHERE id_post = $postId";
+          $resultOldImage = $conn->query($queryOldImage);
+        if ($resultOldImage->num_rows > 0) {
+            $oldImage = $resultOldImage->fetch_assoc()['image_path'];
+            if (file_exist($oldImage)) {
+                unlink($oldImage);
+            }
+        }
+    } else {
+        $imagePathQuery = "SELECT image_path FROM posts WHERE id_post = $postId";
+         $result = $conn->query($imagePathQuery);
+          $imagePath = ($result->num_rows > 0) ? $result->fetch_assoc() ['image_path'] : null;
+    }
+
+    $queryUpdate = "UPDATE posts SET post_title = '$postTitle', content = '$content', category_id = $categoryId, image_path = '$imagePath' WHERE id_post = $postId";
+
+    if ($conn->query($queryUpdate) === TRUE) {
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'Postingan berhasil diperbarui.'
+        ];
+    } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Gagal memperbarui postingan.'
+        ];
+    }
+    header('Location: dashboard.php');
+    exit();
+}
